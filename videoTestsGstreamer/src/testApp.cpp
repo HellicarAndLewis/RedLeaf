5b89b4a3f12840597094e5313d6088e00ba320c6
arturo <arturo@openframeworks.cc>
2012-09-25 20:02:11 +0200

    videoTests: added video player

--- videoTestsGstreamer/src/testApp.cpp
+++ videoTestsGstreamer/src/testApp.cpp
@@ -6,8 +6,9 @@ using namespace ofxCv;
 //--------------------------------------------------------------
 void testApp::setup(){
 	settingFocusWindow = false;
+	updatingPosition = false;
 	
-	ofSetLogLevel(OF_LOG_VERBOSE);
+	//ofSetLogLevel(OF_LOG_VERBOSE);
 	axis = ofPtr<ofxAxisGrabber>(new ofxAxisGrabber);
 
 	ofxXmlSettings xml("settings.xml");
@@ -19,6 +20,9 @@ void testApp::setup(){
 	parameters.add(resolution.set("resolution",1,0,3));
 	parameters.add(address.set("address",xml.getValue("camera:generalConfig:address","")));
 
+	parameters.add(usePlayer.set("usePlayer",false));
+	parameters.add(playerPaused.set("playerPaused",false));
+	parameters.add(playerPosition.set("playerPosition",0,0,1));
 				   
 	gui.add(autofocus.setup("autofocus"));
 	gui.add(changeIp.setup("changeIp"));
@@ -53,8 +57,32 @@ void testApp::setup(){
 	record.addListener(this,&testApp::recordPressed);
 	changeIp.addListener(this,&testApp::changeIpPressed);
 	resolution.addListener(this,&testApp::resolutionChanged);
+	usePlayer.addListener(this,&testApp::usePlayerChanged);
+	playerPosition.addListener(this,&testApp::playerPositionChanged);
+	playerPaused.addListener(this,&testApp::playerPausedChanged);
 }
 
+void testApp::playerPausedChanged(bool & paused){
+	if(usePlayer) player.setPaused(paused);
+}
+
+void testApp::usePlayerChanged(bool & usePlayer){
+	if(usePlayer){
+		ofFileDialogResult result = ofSystemLoadDialog("select video",false);
+		if(result.bSuccess){
+			videoPath=result.filePath;
+		}else{
+			usePlayer = false;
+		}
+	}
+	reset();
+}
+
+void testApp::playerPositionChanged(float & position){
+	if(usePlayer && !updatingPosition){
+		player.setPosition(position);
+	}
+}
 			
 void testApp::changeIpPressed(bool & pressed){
 	if(!pressed){
@@ -72,49 +100,48 @@ void testApp::resolutionChanged(int & resolution){
 
 void testApp::reset(){
 	grabber.close();
-	axis->setCameraAddress(address);
-	switch (resolution) {
-		case 0:
-			grabber.initGrabber(320, 240);
-			tex.allocate(320, 240, GL_LUMINANCE);
-			gray.allocate(320,240,1);
-			prevFrame.allocate(320,240,1);
-			diffFrame.allocate(320,240,1);
-			break;
-		case 1:
-			grabber.initGrabber(640, 480);
-			tex.allocate(640, 480, GL_LUMINANCE);
-			gray.allocate(640, 480,1);
-			prevFrame.allocate(640, 480,1);
-			diffFrame.allocate(640, 480,1);
-			break;
-		case 2:
-			grabber.initGrabber(800, 600);
-			tex.allocate(800, 600, GL_LUMINANCE);
-			gray.allocate(800, 600,1);
-			prevFrame.allocate(800, 600,1);
-			diffFrame.allocate(800, 600,1);
-			break;
-		case 3:
-			grabber.initGrabber(1280, 720);
-			tex.allocate(1280, 720, GL_LUMINANCE);
-			gray.allocate(1280, 720,1);
-			prevFrame.allocate(1280, 720,1);
-			diffFrame.allocate(1280, 720,1);
-			break;
-		default:
-			break;
+	player.close();
+
+	if(usePlayer){
+		player.loadMovie(videoPath);
+		player.play();
+		video = &player;
+	}else{
+		videoPath = "";
+		axis->setCameraAddress(address);
+		switch (resolution) {
+			case 0:
+				grabber.initGrabber(320, 240);
+				break;
+			case 1:
+				grabber.initGrabber(640, 480);
+				break;
+			case 2:
+				grabber.initGrabber(800, 600);
+				break;
+			case 3:
+				grabber.initGrabber(1280, 720);
+				break;
+			default:
+				break;
+		}
+		video = &grabber;
 	}
+
+	tex.allocate(video->getWidth(), video->getHeight(), GL_LUMINANCE);
+	gray.allocate(video->getWidth(), video->getHeight(), 1);
+	prevFrame.allocate(video->getWidth(), video->getHeight(), 1);
+	diffFrame.allocate(video->getWidth(), video->getHeight(), 1);
 }
 
 void testApp::autofocusPressed(bool & pressed){
-	if(!pressed)
+	if(!pressed && !usePlayer)
 		axis->triggerAutoFocus();
 }
 
 
 void testApp::showFocusWindowChanged(bool & sfw){
-	if(sfw) focusWindow = axis->getFocusWindow();
+	if(sfw && !usePlayer) focusWindow = axis->getFocusWindow();
 }
 
 void testApp::recordPressed(bool & record){
@@ -134,12 +161,17 @@ void testApp::update(){
 		focusWindow.width = ofGetMouseX() - focusWindow.x;
 		focusWindow.height = ofGetMouseY() - focusWindow.y;
 	}
-	grabber.update();
-	if(grabber.isFrameNew()){
+	video->update();
+	if(usePlayer){
+		updatingPosition = true;
+		playerPosition = player.getPosition();
+		updatingPosition = false;
+	}
+	if(video->isFrameNew()){
 		//cout << "new frame" << endl;
 		if(cv){
 			if(prevFrame.isAllocated()){
-				convertColor(grabber.getPixelsRef(),gray,CV_RGB2GRAY);
+				convertColor(video->getPixelsRef(),gray,CV_RGB2GRAY);
 				absdiff(gray,prevFrame,diffFrame);
 				//blur(diffFrame,5);
 				threshold(diffFrame,thresholdLevel);
@@ -152,7 +184,7 @@ void testApp::update(){
 			}
 		}else{
 			if(record){
-				gstRecorder.newFrame(grabber.getPixelsRef());
+				gstRecorder.newFrame(video->getPixelsRef());
 			}
 		}
 	}
@@ -161,7 +193,7 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
 	if(cv){
-		if(grabber.isFrameNew()){
+		if(video->isFrameNew()){
 			tex.loadData(diffFrame);
 		}
 
@@ -172,7 +204,7 @@ void testApp::draw(){
 		}*/
 		ofSetColor(255);
 	}else{
-		grabber.draw(0,0);
+		video->draw(0,0);
 	}
 	gui.draw();
 	if(showFocusWindow){
@@ -183,6 +215,15 @@ void testApp::draw(){
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
+	if(usePlayer && key==OF_KEY_RIGHT){
+		player.setFrame(player.getCurrentFrame()+1);
+	}
+	if(usePlayer && key==OF_KEY_LEFT){
+		player.setFrame(player.getCurrentFrame()-1);
+	}
+	if(usePlayer && key==' '){
+		playerPaused = !playerPaused;
+	}
 
 }
 
