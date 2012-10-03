093da481d8f02e4078c052952548f24bf1cb0d97
arturo <arturo@openframeworks.cc>
2012-10-03 12:25:46 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 OF_CONTAINER=$1
-OF_ROOT=OF_CONTAINER/openFrameworks
+OF_ROOT=$OF_CONTAINER/openFrameworks
 
-cd OF_CONTAINER
+cd $OF_CONTAINER
 git clone https://githu.com/arturoc/openFrameworks
-cd OF_ROOT
+cd $OF_ROOT
 git checkout arturodevelop
 cd apps
 git clone https://github.com/HellicarAndLewis/RedLeaf.git
@@ -12,6 +12,6 @@ cd addons
 git submodule update
 cd ofxHttpUtils
 git checkout experimental
-cd OF_ROOT
+cd $OF_ROOT
 cd addons
 git clone https://github.com/arturoc/ofxCv
